302dd00ec50c26bc881694d966ea62092083952e
AlexanderMezhenskyi <alexmezhenskyi@gmail.com>
2017-07-02 23:12:28 +0300

    done

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano')
+const sass = require('gulp-sass');
+
+const stylesheetsSources = './src/assets/stylesheets/**/*.scss';
+
+const rootSources = ['./src/*.html', './src/*.png'];
+const imagesSources = './src/assets/img/**/*';
+const fontsSources = './src/assets/fonts/**/*';
+
+
+const sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'expanded'
+};
+
+
+
+let publishApplication = (destinationDir) => {
+    publishRootFiles(destinationDir);
+    publishImages(destinationDir);
+	publishFonts(destinationDir);
+    publishCssAndAddBrowserPrefixes(destinationDir);
+};
+
+let publishRootFiles = (destinationDir) => {
+    gulp.src(rootSources).pipe(gulp.dest(destinationDir))
+};
+
+let publishImages = (destinationDir) => {
+    gulp.src(imagesSources).pipe(gulp.dest(destinationDir +'assets/img'))
+};
+
+let publishFonts = (destinationDir) => {
+	gulp.src(fontsSources).pipe(gulp.dest(destinationDir +'assets/fonts'))
+};
+
+let publishCssAndAddBrowserPrefixes = (destinationDir) => {
+    let processors = [
+        autoprefixer({
+            remove: false,
+        }),
+        cssnano({
+            discardUnused: {
+                fontFace: false
+            }
+        }),
+    ];
+
+    return gulp.src(stylesheetsSources)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(destinationDir + 'assets/stylesheets'))
+};
+
+
+gulp.task('publish', () => {
+    return publishApplication('dist/')
+});
+
+gulp.task('publish-css', () => {
+    return publishCssAndAddBrowserPrefixes('dist/')
+});
+
+gulp.task('watch', () => {
+    let destinationDir = 'src/';
+    publishCssAndAddBrowserPrefixes(destinationDir);
+
+    return gulp.watch([stylesheetsSources], () => publishCssAndAddBrowserPrefixes(destinationDir))
+});
+
+
+gulp.task('default', ['watch']);
